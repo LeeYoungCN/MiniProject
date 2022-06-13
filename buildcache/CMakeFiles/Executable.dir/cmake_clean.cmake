f566c8d88461fefe72b92ae9e5689c131db9b786
LiYang <1175529741@qq.com>
2022-06-13 22:17:42 +0800

    123

---
+++ buildcache/CMakeFiles/Executable.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../output/Executable.exe"
+  "../output/Executable.exe.manifest"
+  "../output/Executable.pdb"
+  "../output/libExecutable.dll.a"
+  "CMakeFiles/Executable.dir/src/main.cpp.obj"
+  "CMakeFiles/Executable.dir/src/main.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
